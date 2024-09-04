3b73c9e947d2dfbef84bf5b416fef487e4640a19
MarkusTech <wm.recopelacion@gmail.com>
2024-09-04 16:39:58 +0800

    backend setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,22 +4,26 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import bodyParser from "body-parser";
+import "colors"; // Import the colors package and use its prototype methods
 
 // Load environment variables from .env file
 dotenv.config();
 
 // REST OBJ
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Middleware setup
-app.use(morgan("dev")); // Logging
+app.use(morgan("dev"));
 app.use(cors());
 app.use(helmet());
-app.use(bodyParser.json()); //
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Example colored output
+console.log("Starting server...".green);
+
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`.yellow.bold);
 });
