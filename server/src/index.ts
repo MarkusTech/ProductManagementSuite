b35d5a6e5aa5e15fb38c9368d5d736f73745b335
MarkusTech <wm.recopelacion@gmail.com>
2024-09-04 16:35:38 +0800

    backend setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,23 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import bodyParser from "body-parser";
 
+// Load environment variables from .env file
+dotenv.config();
+
+// REST OBJ
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
+// Middleware setup
+app.use(morgan("dev")); // Logging
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json()); //
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.listen(port, () => {
