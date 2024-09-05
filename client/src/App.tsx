3a1bd22d3c08add9a20361847b5577e7f12e2955
MarkusTech <wm.recopelacion@gmail.com>
2024-09-05 13:54:05 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
+import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar /> {/* Use the Navbar component */}
       <div className="text-red-500">Hello World!</div>
     </>
   );
