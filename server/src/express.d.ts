c5036db32749b67ef026c495abf9aeb0f5ea8f60
MarkusTech <wm.recopelacion@gmail.com>
2024-09-23 13:44:26 +0800

    Update express.d.ts

--- server/src/express.d.ts
+++ server/src/express.d.ts
@@ -1,10 +1,9 @@
-// src/@types/express.d.ts
-import { User as UserModel } from "../models/User"; // Adjust the import based on your User model location
+import { User as UserModel } from "../models/User";
 
 declare global {
   namespace Express {
     interface Request {
-      user?: UserModel; // Assuming UserModel is the type of your user object
+      user?: UserModel;
     }
   }
 }
