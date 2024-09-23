9a5ceb51499727fa1c41615c17e6610c113bafa8
MarkusTech <wm.recopelacion@gmail.com>
2024-09-23 14:11:40 +0800

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,19 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import bodyParser from "body-parser";
-import "colors"; // Import the colors package and use its prototype methods
+import "colors";
+import connectDB from "./database/connect";
+import errorHandler from "./middleware/errorsMiddleware";
 
 // Load environment variables from .env file
 dotenv.config();
 
 // REST OBJ
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Middleware setup
 app.use(morgan("dev"));
@@ -21,7 +23,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Example colored output
+// CORS middleware
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  next();
+});
+
+// Route handlers
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/category", require("./routes/categoryRoutes"));
+app.use("/api/product", require("./routes/productRoutes"));
+app.use("/api/upload", require("./utils/uploadImage"));
+
+app.use(errorHandler);
+connectDB();
+
 console.log("Starting server...".green);
 
 app.listen(port, () => {
