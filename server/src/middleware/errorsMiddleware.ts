486a1d35b5f8b4f7604dac590f1ef6c62fca20d5
MarkusTech <wm.recopelacion@gmail.com>
2024-09-23 13:45:04 +0800

    Update errorsMiddleware.ts

---
+++ server/src/middleware/errorsMiddleware.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+
+interface CustomError extends Error {
+  status?: number; // Optional status property
+}
+
+const errorHandler = (
+  err: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode = err.status || 500;
+  res.status(statusCode).json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "development" ? err.stack : null,
+  });
+};
+
+export default errorHandler;
