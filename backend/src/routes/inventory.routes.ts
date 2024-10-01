958211581f1aa8ab52ce6ac7cd010a2102ced517
MarkusTech <wm.recopelacion@gmail.com>
2024-10-01 10:32:09 +0800

    Create inventory.routes.ts

---
+++ backend/src/routes/inventory.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { InventoryController } from "../controller/inventoryController";
+import { verifyToken } from "../middlewares/authMiddlewares";
+
+const router = Router();
+const inventoryController = new InventoryController();
+
+router.post("/inventory", verifyToken, inventoryController.createInventory);
+router.get("/inventory", verifyToken, inventoryController.getAllInventory);
+router.get(
+  "/inventory/:inventoryID",
+  verifyToken,
+  inventoryController.getInventoryById
+);
+router.put(
+  "/inventory/:inventoryID",
+  verifyToken,
+  inventoryController.updateInventory
+);
+router.delete(
+  "/inventory/:inventoryID",
+  verifyToken,
+  inventoryController.deleteInventory
+);
+
+export default router;
