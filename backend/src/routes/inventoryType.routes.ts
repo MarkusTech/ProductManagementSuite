dcf5f3b478048fcbe55047d5c1d205fb70d0b877
MarkusTech <wm.recopelacion@gmail.com>
2024-10-01 10:58:39 +0800

    Update inventoryType.routes.ts

--- backend/src/routes/inventoryType.routes.ts
+++ backend/src/routes/inventoryType.routes.ts
@@ -9,26 +9,26 @@ router.post(
   "/inventory-type",
   verifyToken,
   inventoryTypeController.createInventoryType
-); // Create a new InventoryType
+);
 router.get(
   "/inventory-type",
   verifyToken,
   inventoryTypeController.getAllInventoryTypes
-); // Get all InventoryTypes
+);
 router.get(
   "/inventory-type/:inventoryTypeID",
   verifyToken,
   inventoryTypeController.getInventoryTypeById
-); // Get InventoryType by ID
+);
 router.put(
   "/inventory-type/:inventoryTypeID",
   verifyToken,
   inventoryTypeController.updateInventoryType
-); // Update InventoryType
+);
 router.delete(
   "/inventory-type/:inventoryTypeID",
   verifyToken,
   inventoryTypeController.deleteInventoryType
-); // Delete InventoryType
+);
 
 export default router;
