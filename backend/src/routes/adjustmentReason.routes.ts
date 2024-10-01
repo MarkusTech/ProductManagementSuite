12a326933a128537043315f7886b40cea851a745
MarkusTech <wm.recopelacion@gmail.com>
2024-10-01 10:57:32 +0800

    Update adjustmentReason.routes.ts

--- backend/src/routes/adjustmentReason.routes.ts
+++ backend/src/routes/adjustmentReason.routes.ts
@@ -9,26 +9,26 @@ router.post(
   "/adjustment-reason",
   verifyToken,
   adjustmentReasonController.createAdjustmentReason
-); // Create a new AdjustmentReason
+);
 router.get(
   "/adjustment-reason",
   verifyToken,
   adjustmentReasonController.getAllAdjustmentReasons
-); // Get all AdjustmentReasons
+);
 router.get(
   "/adjustment-reason/:adjustmentReasonID",
   verifyToken,
   adjustmentReasonController.getAdjustmentReasonById
-); // Get AdjustmentReason by ID
+);
 router.put(
   "/adjustment-reason/:adjustmentReasonID",
   verifyToken,
   adjustmentReasonController.updateAdjustmentReason
-); // Update AdjustmentReason
+);
 router.delete(
   "/adjustment-reason/:adjustmentReasonID",
   verifyToken,
   adjustmentReasonController.deleteAdjustmentReason
-); // Delete AdjustmentReason
+);
 
 export default router;
