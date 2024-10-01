e7b597297a6bcc886bdcb66007a88cd9ebf911f7
MarkusTech <wm.recopelacion@gmail.com>
2024-10-01 10:58:22 +0800

    Update inventoryAdjustment.routes.ts

--- backend/src/routes/inventoryAdjustment.routes.ts
+++ backend/src/routes/inventoryAdjustment.routes.ts
@@ -9,26 +9,26 @@ router.post(
   "/inventory-adjustment",
   verifyToken,
   inventoryAdjustmentController.createInventoryAdjustment
-); // Create a new InventoryAdjustment
+);
 router.get(
   "/inventory-adjustment",
   verifyToken,
   inventoryAdjustmentController.getAllInventoryAdjustments
-); // Get all InventoryAdjustments
+);
 router.get(
   "/inventory-adjustment/:adjustmentID",
   verifyToken,
   inventoryAdjustmentController.getInventoryAdjustmentById
-); // Get InventoryAdjustment by ID
+);
 router.put(
   "/inventory-adjustment/:adjustmentID",
   verifyToken,
   inventoryAdjustmentController.updateInventoryAdjustment
-); // Update InventoryAdjustment
+);
 router.delete(
   "/inventory-adjustment/:adjustmentID",
   verifyToken,
   inventoryAdjustmentController.deleteInventoryAdjustment
-); // Delete InventoryAdjustment
+);
 
 export default router;
