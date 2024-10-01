8d5b460fca80946553b562b80c7a282a23b5cd81
MarkusTech <wm.recopelacion@gmail.com>
2024-10-01 10:12:22 +0800

    Update category.routes.ts

--- backend/src/routes/category.routes.ts
+++ backend/src/routes/category.routes.ts
@@ -5,22 +5,22 @@ import { verifyToken } from "../middlewares/authMiddlewares";
 const router = Router();
 const categoryController = new CategoryController();
 
-router.post("/categories", verifyToken, categoryController.createCategory); // Protected route
-router.get("/categories", verifyToken, categoryController.getAllCategories); // Protected route
+router.post("/categories", verifyToken, categoryController.createCategory);
+router.get("/categories", verifyToken, categoryController.getAllCategories);
 router.get(
   "/categories/:categoryID",
   verifyToken,
   categoryController.getCategoryById
-); // Protected route
+);
 router.put(
   "/categories/:categoryID",
   verifyToken,
   categoryController.updateCategory
-); // Protected route
+);
 router.delete(
   "/categories/:categoryID",
   verifyToken,
   categoryController.deleteCategory
-); // Protected route
+);
 
 export default router;
