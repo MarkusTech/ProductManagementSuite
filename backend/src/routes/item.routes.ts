8f3a28173b27adb49c08907541e6121a437c2243
MarkusTech <wm.recopelacion@gmail.com>
2024-10-01 10:58:56 +0800

    Update item.routes.ts

--- backend/src/routes/item.routes.ts
+++ backend/src/routes/item.routes.ts
@@ -5,10 +5,10 @@ import { verifyToken } from "../middlewares/authMiddlewares";
 const router = Router();
 const itemController = new ItemController();
 
-router.post("/items", verifyToken, itemController.createItem); // Create new item
-router.get("/items", verifyToken, itemController.getAllItems); // Get all items
-router.get("/items/:itemID", verifyToken, itemController.getItemById); // Get item by ID
-router.put("/items/:itemID", verifyToken, itemController.updateItem); // Update item
-router.delete("/items/:itemID", verifyToken, itemController.deleteItem); // Delete item
+router.post("/items", verifyToken, itemController.createItem);
+router.get("/items", verifyToken, itemController.getAllItems);
+router.get("/items/:itemID", verifyToken, itemController.getItemById);
+router.put("/items/:itemID", verifyToken, itemController.updateItem);
+router.delete("/items/:itemID", verifyToken, itemController.deleteItem);
 
 export default router;
