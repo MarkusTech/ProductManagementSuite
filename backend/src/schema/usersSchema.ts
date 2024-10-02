55abbcf47bf26609359e0c8a5979db98769ef0cb
MarkusTech <wm.recopelacion@gmail.com>
2024-10-02 16:02:50 +0800

    Update usersSchema.ts

--- backend/src/schema/usersSchema.ts
+++ backend/src/schema/usersSchema.ts
@@ -1,21 +1,34 @@
 import { z } from "zod";
 
-export const UserSchema = z.object({
-  userID: z.number().int().optional(),
-  firstName: z.string().nonempty(),
+export const CreateUserSchema = z.object({
+  firstName: z.string().min(1, "First name is required"),
   middleName: z.string().optional(),
-  lastName: z.string().nonempty(),
-  roleID: z.number().int(),
-  username: z.string().nonempty(),
-  email: z.string().email().nonempty(),
-  password: z.string().nonempty(),
+  lastName: z.string().min(1, "Last name is required"),
+  roleID: z.number().int().positive(),
+  username: z
+    .string()
+    .min(1, "Username is required")
+    .max(20, "Username must be at most 20 characters"),
+  email: z.string().email("Invalid email format"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+  phoneNumber: z.string().optional(),
+  address: z.string().optional(),
+  birthday: z.string().optional(), // You can use z.date() if you want to validate dates
+  createdByID: z.number().int().positive(),
+  modifiedByID: z.number().int().optional(),
+});
+
+export const UpdateUserSchema = z.object({
+  firstName: z.string().optional(),
+  middleName: z.string().optional(),
+  lastName: z.string().optional(),
+  roleID: z.number().int().positive().optional(),
+  username: z.string().optional(),
+  email: z.string().email("Invalid email format").optional(),
   phoneNumber: z.string().optional(),
   address: z.string().optional(),
   birthday: z.string().optional(),
-  status: z.boolean().default(true),
+  status: z.boolean().optional(),
   image_url: z.string().optional(),
-  createdByID: z.number().int(),
   modifiedByID: z.number().int().optional(),
-  createdAt: z.date().optional(),
-  updatedAt: z.date().optional(),
 });
